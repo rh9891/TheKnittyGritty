fdd84e4c088ebd2ba2513318f285a6c02c195346
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-02-01 21:14:17 -0500

    Modified the CSS and layout of the header/navigation bar.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
-  return <header>The Knitty Gritty Header</header>;
+  return (
+    <header>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">The Knitty Gritty</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/Login">Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
