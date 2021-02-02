17a6c34b850b7bc4f86bcf08d79f3b05172517a5
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-02-01 21:26:09 -0500

    Imported the Font Awesome kit. Added icons for the shopping cart and login. Adjusted the positioning of the header.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Fragment>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>The Knitty Gritty</h1>
         </Container>
