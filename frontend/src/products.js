9c15469b80ef9b2eb985c906f6de52ccff0d944d
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-02-02 23:59:17 -0500

    Created a file for the application's products.

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Carroll Coorie",
+    image: "/images/wild-mustard.jpg",
+    description:
+      "100% super kid mohair from Scotland.Though less fluffier than the usual mohair-silk blend, Carroll Coorie is incredibly soft with a gentle halo and an exquisite shimmer that has the density and drape of alpaca with a natural luster.",
+    content: "Mohair",
+    category: "Yarn",
+    price: 19.5,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Woolf Wooly Wool",
+    image: "/images/blossom-pink.jpg",
+    description:
+      "100% wool. Woolf Wooly Wool is a single-ply super bulky blend with a soft and gentle beauty. Great for making sweaters, warm wraps, toasty blankets and cozy hats.",
+    content: "Wool",
+    category: "Yarn",
+    price: 28.5,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Austen Alpaca",
+    image: "/images/dark-iris.jpg",
+    description:
+      "Made up of 43% baby alpaca, 42% extra fine merino, and 15% linen. Austen Alpaca is a very special yarn, full of gentle charm and natural beauty. The linen is undyed, casting the entire palette in a soft, tweedy haze.",
+    content: "Alpaca/Merino/Linen",
+    category: "Yarn",
+    price: 23.5,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Kipling Cashmere",
+    image: "/images/heather.jpg",
+    description:
+      "75% extra fine merino wool and 25% cashmere. Kipling Cashmere is a classic plied yarn. With a quarter of its components being cashmere, it is incredibly soft and supple. The remaining elements are of extra fine merino wool, so it is elastic and durable. A light worsted weight, Kipling Cashmere is perfect for everything and everyone, gentle for babies and sensitive skin.",
+    content: "Merino/Cashmere",
+    category: "Yarn",
+    price: 30.6,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Wilde Worsted Twist",
+    image: "/images/aqua-cove.jpg",
+    description:
+      "100% merino wool. Wilde Worsted Twist is a classic 2-ply farm yarn. It is as soft as a baby lamb's ear and perfect for cowls, scarves, hats, mittens, sweaters, and blankets.",
+    content: "Merino",
+    category: "Yarn",
+    price: 22.0,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Lawrence Linen",
+    image: "/images/high-tide.jpg",
+    description:
+      "50% fine highland wool, 35% alpaca, and 15% linen. Lawrence Linen is a fingering weight blend of fine wool, alpaca, and linen. The yarn is windswept and elegant, hearty and beautiful. These fibers are spectacular for knitting, crocheting, and weaving and can be used for garments, blankets, accessories, and housewares.",
+    content: "Wool/Alpaca/Linen",
+    category: "Yarn",
+    price: 17.5,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
