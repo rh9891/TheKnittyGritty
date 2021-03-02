a09d629b8b9ee5e9529238a58c8a8c734b915203
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-02 15:39:37 -0500

    Called in axios. Brought in the useState and useEffect hooks for the home screen products. With useEffect, I called in products from the backend. Added a proxy to avoid any errors across the domains.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import products from "../products";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <Fragment>
       <h1>Latest Products</h1>
