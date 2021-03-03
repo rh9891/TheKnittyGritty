50f139735dbd566f73c6d463a2849330347ed295
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-02 22:57:32 -0500

    Made additional syntax changes to the single page product file, but still having issues rendering all wanted information.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -55,7 +55,7 @@ const ProductScreen = ({ match }) => {
                 <Row>
                   <Col>Price:</Col>
                   <Col>
-                    <strong>{product.price}</strong>
+                    <strong>${product.price}</strong>
                   </Col>
                 </Row>
               </ListGroup.Item>
