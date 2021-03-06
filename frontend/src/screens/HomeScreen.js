ec69bf3fd0f3a0949648c725e9d655839b625792
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-05 21:47:50 -0500

    Created a functionality to send a message to the user on the HomeScreen component about page loading when loading and to notify the user of any errors on the backend.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -17,15 +17,21 @@ const HomeScreen = () => {
   return (
     <Fragment>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <h3>
-              <Product product={product} />
-            </h3>
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <h3>
+                <Product product={product} />
+              </h3>
+            </Col>
+          ))}
+        </Row>
+      )}
     </Fragment>
   );
 };
