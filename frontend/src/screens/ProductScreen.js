b09668f11c2b03d3c51906ba97fad594cc546e1e
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-06 09:01:21 -0500

    Completed bringing in the action for the product screen component, so that the product details has been added to Redux. Application is working as expected after verifying Redux dev tools.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,87 +1,94 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { listProductDetails } from "../actions/productActions";
 
 const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`);
-
-      setProduct(data);
-    };
-
-    fetchProduct();
-  }, [match]);
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
   return (
     <Fragment>
       <Link className="btn btn-primary my-3" to="/">
         Go Back
       </Link>
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h2>{product.name}</h2>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>{product.description}</ListGroup.Item>
-            <ListGroup.Item>
-              <li>Weight: {product.weight}</li>
-              <li>Length: {product.length}</li>
-              <li>Gauge: {product.gauge}</li>
-              <li>Knitting Needle Size: {product.knitting_needle}</li>
-              <li>Crochet Hook Size: {product.crochet_hook}</li>
-              <li>Recommended Care: {product.recommended_care}</li>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h2>{product.name}</h2>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col>Availability:</Col>
-                  <Col>
-                    <strong>
-                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
-                    </strong>
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
               </ListGroup.Item>
+              <ListGroup.Item>{product.description}</ListGroup.Item>
               <ListGroup.Item>
-                <Button
-                  className="btn-block"
-                  type="button"
-                  disabled={product.countInStock === 0}
-                >
-                  Add to Cart
-                </Button>
+                <li>Weight: {product.weight}</li>
+                <li>Length: {product.length}</li>
+                <li>Gauge: {product.gauge}</li>
+                <li>Knitting Needle Size: {product.knitting_needle}</li>
+                <li>Crochet Hook Size: {product.crochet_hook}</li>
+                <li>Recommended Care: {product.recommended_care}</li>
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Availability:</Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button
+                    className="btn-block"
+                    type="button"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </Fragment>
   );
 };
