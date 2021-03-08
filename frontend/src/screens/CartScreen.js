f73ae83588b8b8b56da17650aa119fff3282aa86
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-07 20:38:19 -0500

    Created a new component for the cart. Imported the CartScreen into the App.js and created a route for the page.

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const CartScreen = () => {
+  return (
+    <Fragment>
+      <div>This is the cart.</div>
+    </Fragment>
+  );
+};
+
+export default CartScreen;
