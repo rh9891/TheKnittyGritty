8b790e4afa9c0a60460a2b85e5dc7754e8a16567
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-08 08:17:40 -0500

    Added functionality to the cart screen component so that when an item is added to the cart, it is rendered on the DOM. Using the Redux Dev Tools, the addition of the cart items is also reflected in the store as actions.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -29,9 +29,65 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productID, qty]);
 
+  const removeFromCartHandler = (id) => {
+    console.log("Item has been successfully removed.");
+  };
+
   return (
     <Fragment>
-      <div>This is the cart.</div>
+      <Row>
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          {cartItems.length === 0 ? (
+            <Message>
+              Knit happens! Looks like your cart is currently empty.&nbsp;
+              <Link to="/">Continue shopping?</Link>
+            </Message>
+          ) : (
+            <ListGroup variant="flush">
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item.product}>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col md={3}>
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={2}>${item.price}</Col>
+                    <Col md={2}>
+                      <Form.Control
+                        as="select"
+                        value={item.qty}
+                        onChange={(event) =>
+                          dispatch(addToCart(item.product, event.target.value))
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                    <Col md={2}>
+                      <Button
+                        type="button"
+                        variant="secondary"
+                        onClick={() => removeFromCartHandler(item.product)}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+        <Col md={2}></Col>
+        <Col md={2}></Col>
+      </Row>
     </Fragment>
   );
 };
