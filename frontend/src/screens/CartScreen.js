249dc063ef0b88411a25df62c76e2fa98570b333
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-08 11:35:19 -0500

    Redirected the button (to place order) to the login page. If the user is logged in, the application will redirect the user to the shipping page.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -34,7 +34,7 @@ const CartScreen = ({ match, location, history }) => {
   };
 
   const checkoutHandler = () => {
-    console.log("You have placed an order.");
+    history.push("/login?redirect=shipping");
   };
 
   return (
@@ -58,7 +58,7 @@ const CartScreen = ({ match, location, history }) => {
                     <Col md={3}>
                       <Link to={`/product/${item.product}`}>{item.name}</Link>
                     </Col>
-                    <Col md={2}>${item.price}</Col>
+                    <Col md={2}>${item.price.toFixed(2)}</Col>
                     <Col md={2}>
                       <Form.Control
                         as="select"
