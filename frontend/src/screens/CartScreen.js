d1409c6a523526d3c4359a71cf4f738f04f7f560
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-07 23:44:26 -0500

    Added functionality to Add to Cart button. When items are added, they are successfully stored to localStorage. Using the Redux Dev Tools, I verify that the actions are successfully dispatched to store.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,31 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+  const productID = match.params.id;
+
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productID) {
+      dispatch(addToCart(productID, qty));
+    }
+  }, [dispatch, productID, qty]);
+
   return (
     <Fragment>
       <div>This is the cart.</div>
