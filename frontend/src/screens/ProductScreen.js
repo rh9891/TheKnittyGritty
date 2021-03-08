018c77be1ce05fd453bf889b760913e959a85bb1
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-07 20:45:14 -0500

    Fixed a syntax error. Initialized the quantity to be 1 instead of 0 as the lowest possible value for the quantity being bought is 1.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,7 +16,7 @@ import Message from "../components/Message";
 import { listProductDetails } from "../actions/productActions";
 
 const ProductScreen = ({ history, match }) => {
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(1);
 
   const dispatch = useDispatch();
 
