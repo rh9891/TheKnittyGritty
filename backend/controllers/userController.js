781863784590897f7d9ae2c2e0fce1d267f92b9d
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-08 22:47:18 -0500

    Created a routes file for the user. Brought in the routes file for the user into the server.js file. Fixed syntax error in the user controllers file. Tested backend successfully through Postman.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,10 @@ import User from "../models/userModel.js";
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  res.send(email, password);
+  res.send({
+    email,
+    password,
+  });
 });
 
 export { authUser };
