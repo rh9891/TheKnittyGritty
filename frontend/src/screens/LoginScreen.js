bdc2381381357fae74bc12ab0e7e7e95819cf08b
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-09 14:27:58 -0500

    Created two new components for the form container and the login page. Added the login component to the App.js file.

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { login } from "../actions/userActions";
+
+const LoginScreen = ({ location }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // Login is dispatched.
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          Don't have an account?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
+            Sign up.
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
