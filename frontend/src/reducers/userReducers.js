390659bfe544e7b1b49a1d4a7a0ff91030ea5bac
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-10 00:02:32 -0500

    Created reducers for when updating the user's profile.

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -10,6 +10,9 @@ import {
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
   USER_DETAILS_RESET,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -50,6 +53,19 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
       return { loading: false, error: action.payload };
     case USER_DETAILS_RESET:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true };
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true, userInfo: action.payload };
+    case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
