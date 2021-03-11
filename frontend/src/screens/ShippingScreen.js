fa9aa9e9d6065de56e53ee4f5e1650b223ad929f
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-10 23:17:16 -0500

    Created a component for the shipping page.

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+
+const ShippingScreen = ({ history }) => {
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [country, setCountry] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("Shipping address is submitted.");
+  };
+
+  return (
+    <FormContainer>
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="address">
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type="text"
+            value={address}
+            required
+            onChange={(event) => setAddress(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="city">
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type="text"
+            value={city}
+            required
+            onChange={(event) => setCity(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="postalCode">
+          <Form.Label>Postal Code</Form.Label>
+          <Form.Control
+            type="text"
+            value={postalCode}
+            required
+            onChange={(event) => setPostalCode(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="country">
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            type="text"
+            value={country}
+            required
+            onChange={(event) => setCountry(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default ShippingScreen;
