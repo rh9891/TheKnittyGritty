c8890defd2b8b4695f4925978a6448bba19b2462
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-16 18:21:50 -0400

    Changed the form-group location in the code in order to fix the formatting of the page. Commented out the Stripe option to later implement it.

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -30,28 +30,29 @@ const PaymentScreen = ({ history }) => {
       <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label as="legend">Choose a payment method.</Form.Label>
-        </Form.Group>
-        <Col>
-          <Form.Check
-            type="radio"
-            label="Credit Card or PayPal"
-            id="PayPal"
-            name="paymentMethod"
-            value="PayPal"
-            checked={paymentMethod === "PayPal"}
-            onChange={(event) => setPaymentMethod(event.target.value)}
-          ></Form.Check>
+          <Col>
+            <Form.Check
+              type="radio"
+              label="Credit Card or PayPal"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked={paymentMethod === "PayPal"}
+              onChange={(event) => setPaymentMethod(event.target.value)}
+            ></Form.Check>
 
-          <Form.Check
-            type="radio"
-            label="Stripe"
-            id="Stripe"
-            name="paymentMethod"
-            value="Stripe"
-            checked={paymentMethod === "Stripe"}
-            onChange={(event) => setPaymentMethod(event.target.value)}
-          ></Form.Check>
-        </Col>
+            {/* <Form.Check
+              type="radio"
+              label="Stripe"
+              id="Stripe"
+              name="paymentMethod"
+              value="Stripe"
+              checked={paymentMethod === "Stripe"}
+              onChange={(event) => setPaymentMethod(event.target.value)}
+            ></Form.Check> */}
+          </Col>
+        </Form.Group>
+
         <Button type="submit" variant="primary">
           Continue
         </Button>
