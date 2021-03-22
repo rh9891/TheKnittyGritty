43f2db393747223e72558e0a0c369648ae03dedd
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-21 23:19:17 -0400

    Added functionality to get the order by its ID endpoint. Tested the created route through Postman and it worked successfully.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -35,4 +35,19 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// Route to retrieve an order by ID. GET request to "/api/orders/:id". Private route.
+const getOrderByID = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Sorry. Order was not found.");
+  }
+});
+
+export { addOrderItems, getOrderByID };
