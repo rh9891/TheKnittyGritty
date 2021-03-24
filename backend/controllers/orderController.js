0f2af53ee2808697f337054f44b6c51439d4fdd0
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-24 09:05:44 -0400

    Added another function to order controller to update order status as paid.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,4 +52,29 @@ const getOrderByID = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderByID };
+// Route to update order status as paid. GET request to "/api/orders/:id/pay". Private route.
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error(
+      "We could not retrieve any details for that order number. Please check that you have entered it correctly and try again."
+    );
+  }
+});
+
+export { addOrderItems, getOrderByID, updateOrderToPaid };
