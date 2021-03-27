ef9ae212f9eacd5e7257e5c2bf050fdd8bc9ceec
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-26 21:27:46 -0400

    Added the backend route to retrieve the orders for the individual users. Tested it via Postman successfully.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,10 +4,12 @@ import {
   addOrderItems,
   getOrderByID,
   updateOrderToPaid,
+  getUserOrders,
 } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/myorders").get(protect, getUserOrders);
 router.route("/:id").get(protect, getOrderByID);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
 
