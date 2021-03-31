09e9d288dfd2ff430c159f29fd49ec6a9d6f76a4
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-31 12:07:33 -0400

    Added the admin middleware so that only the admin can access the route to get all users. Tested the backend via Postman successfully.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,9 +7,9 @@ import {
   updateUserProfile,
   getUsers,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(registerUser).get(protect, getUsers);
+router.route("/").post(registerUser).get(protect, isAdmin, getUsers);
 router.post("/login", authUser);
 router
   .route("/profile")
