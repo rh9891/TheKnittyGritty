88ab9fd05e2c313e7ba32966a05db2f16aee88e6
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-04-07 22:38:02 -0400

    Created a component for the admin's products list and brought the page into the app.js file. Checked the admin route for the edit on the frontend and it works as expected. Need to add functionality to create a product and delete a product.

---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,150 @@
+import React, { Fragment, useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import {
+  Table,
+  Button,
+  Row,
+  Col,
+  Popover,
+  OverlayTrigger,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listProducts } from "../actions/productActions";
+
+const ProductListScreen = ({ history, match }) => {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listProducts());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, history, userInfo]);
+
+  const deleteHandler = (id) => {
+    // Functionality to delete a product.
+    console.log("You have successfully deleted a product.");
+  };
+
+  const createProductHandler = (product) => {
+    // Functionality to create a product.
+    console.log("You have successfully created a product.");
+  };
+
+  return (
+    <Fragment>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-right">
+          <Button className="my-3" onClick={createProductHandler}>
+            <i className="fas fa-plus"></i> Create Product
+          </Button>
+        </Col>
+      </Row>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>Product ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Content</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>${product.price.toFixed(2)}</td>
+                <td>{product.category}</td>
+                <td>{product.content}</td>
+                <td>
+                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                    <Button variant="light" className="btn-sm">
+                      <i
+                        className="fas fa-edit"
+                        style={{ color: "#6bc3d4" }}
+                      ></i>
+                    </Button>
+                  </LinkContainer>
+
+                  <OverlayTrigger
+                    rootClose
+                    trigger="click"
+                    placement="left"
+                    overlay={
+                      <Popover
+                        id="popover-basic"
+                        style={{
+                          position: "relative",
+                          justifyContent: "space-evenly",
+                        }}
+                      >
+                        <Popover.Title as="h3">Are you sure?</Popover.Title>
+                        <Popover.Content>
+                          This product will be deleted immediately. You cannot
+                          undo this action.
+                        </Popover.Content>
+                        <Button
+                          variant="secondary"
+                          className="btn-sm"
+                          style={{
+                            position: "relative",
+                            marginLeft: "1.5rem",
+                            marginRight: "1rem",
+                            marginBottom: ".5rem",
+                          }}
+                          onClick={() => document.body.click()}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          variant="primary"
+                          className="btn-sm"
+                          style={{
+                            position: "relative",
+                            marginLeft: ".5rem",
+                            marginRight: "1rem",
+                            marginBottom: ".5rem",
+                          }}
+                          onClick={() => deleteHandler(product._id)}
+                        >
+                          Yes, Delete
+                        </Button>
+                      </Popover>
+                    }
+                  >
+                    <Button variant="danger" className="btn-sm">
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </OverlayTrigger>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </Fragment>
+  );
+};
+
+export default ProductListScreen;
