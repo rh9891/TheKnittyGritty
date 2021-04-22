7a44607db054de7941213aac81bb3e15ac679200
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-04-21 23:28:19 -0400

    Created an endpoint for the product review. Checked the success of the backend route via Postman and everything is working as expected.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -100,10 +100,52 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// Route to create a new review. POST request to "/api/products/:id/reviews". Private route.
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product has already been reviewed.");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res
+      .status(201)
+      .json({ message: "Product review has been successfully added." });
+  } else {
+    res.status(404);
+    throw new Error("Product not found.");
+  }
+});
+
 export {
   getProducts,
   getProductByID,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
