96bc9da6e8ccb8875ea7fe0cca806319d4c396d2
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-04-27 22:36:22 -0400

    Created a search box component for the navbar and imported it into the app.js. Has yet to have functionality.

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+const SearchBox = ({ history }) => {
+  const [keyword, setKeyword] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`);
+    } else {
+      history.push("/");
+    }
+  };
+
+  return (
+    <Form onSubmit={submitHandler} inline>
+      <Form.Control
+        type="text"
+        name="q"
+        onChange={(event) => setKeyword(event.target.value)}
+        className="mr-sm-2 ml-sm-5"
+        placeholder="Search"
+      ></Form.Control>
+      <Button type="submit" className="btn btn-secondary" className="p-2">
+        Search
+      </Button>
+    </Form>
+  );
+};
+
+export default SearchBox;
