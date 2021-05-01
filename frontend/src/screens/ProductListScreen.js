16d0234868776f17403973be5e730e3893ed0eab
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-04-30 20:28:32 -0400

    Added pagination functionality to the product list page for the admin.

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -11,6 +11,7 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
+import Paginate from "../components/Paginate";
 import {
   listProducts,
   deleteProduct,
@@ -19,10 +20,12 @@ import {
 import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
 
 const ProductListScreen = ({ history, match }) => {
+  const pageNumber = match.params.pageNumber || 1;
+
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const { loading, error, products, page, pages } = productList;
 
   const productDelete = useSelector((state) => state.productDelete);
   const {
@@ -52,7 +55,7 @@ const ProductListScreen = ({ history, match }) => {
     if (successCreate) {
       history.push(`/admin/product/${createdProduct._id}/edit`);
     } else {
-      dispatch(listProducts());
+      dispatch(listProducts("", pageNumber));
     }
   }, [
     dispatch,
@@ -61,6 +64,7 @@ const ProductListScreen = ({ history, match }) => {
     successDelete,
     successCreate,
     createdProduct,
+    pageNumber,
   ]);
 
   const deleteHandler = (id) => {
@@ -92,90 +96,93 @@ const ProductListScreen = ({ history, match }) => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Table striped bordered hover responsive className="table-sm">
-          <thead>
-            <tr>
-              <th>Product ID</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Category</th>
-              <th>Content</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr key={product._id}>
-                <td>{product._id}</td>
-                <td>{product.name}</td>
-                <td>${product.price.toFixed(2)}</td>
-                <td>{product.category}</td>
-                <td>{product.content}</td>
-                <td>
-                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                    <Button variant="light" className="btn-sm">
-                      <i
-                        className="fas fa-edit"
-                        style={{ color: "#6bc3d4" }}
-                      ></i>
-                    </Button>
-                  </LinkContainer>
-
-                  <OverlayTrigger
-                    rootClose
-                    trigger="click"
-                    placement="left"
-                    overlay={
-                      <Popover
-                        id="popover-basic"
-                        style={{
-                          position: "relative",
-                          justifyContent: "space-evenly",
-                        }}
-                      >
-                        <Popover.Title as="h3">Are you sure?</Popover.Title>
-                        <Popover.Content>
-                          This product will be deleted immediately. You cannot
-                          undo this action.
-                        </Popover.Content>
-                        <Button
-                          variant="secondary"
-                          className="btn-sm"
-                          style={{
-                            position: "relative",
-                            marginLeft: "1.5rem",
-                            marginRight: "1rem",
-                            marginBottom: ".5rem",
-                          }}
-                          onClick={() => document.body.click()}
-                        >
-                          Cancel
-                        </Button>
-                        <Button
-                          variant="primary"
-                          className="btn-sm"
-                          style={{
-                            position: "relative",
-                            marginLeft: ".5rem",
-                            marginRight: "1rem",
-                            marginBottom: ".5rem",
-                          }}
-                          onClick={() => deleteHandler(product._id)}
-                        >
-                          Yes, Delete
-                        </Button>
-                      </Popover>
-                    }
-                  >
-                    <Button variant="danger" className="btn-sm">
-                      <i className="fas fa-trash"></i>
-                    </Button>
-                  </OverlayTrigger>
-                </td>
+        <Fragment>
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>Product ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Category</th>
+                <th>Content</th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price.toFixed(2)}</td>
+                  <td>{product.category}</td>
+                  <td>{product.content}</td>
+                  <td>
+                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                      <Button variant="light" className="btn-sm">
+                        <i
+                          className="fas fa-edit"
+                          style={{ color: "#6bc3d4" }}
+                        ></i>
+                      </Button>
+                    </LinkContainer>
+
+                    <OverlayTrigger
+                      rootClose
+                      trigger="click"
+                      placement="left"
+                      overlay={
+                        <Popover
+                          id="popover-basic"
+                          style={{
+                            position: "relative",
+                            justifyContent: "space-evenly",
+                          }}
+                        >
+                          <Popover.Title as="h3">Are you sure?</Popover.Title>
+                          <Popover.Content>
+                            This product will be deleted immediately. You cannot
+                            undo this action.
+                          </Popover.Content>
+                          <Button
+                            variant="secondary"
+                            className="btn-sm"
+                            style={{
+                              position: "relative",
+                              marginLeft: "1.5rem",
+                              marginRight: "1rem",
+                              marginBottom: ".5rem",
+                            }}
+                            onClick={() => document.body.click()}
+                          >
+                            Cancel
+                          </Button>
+                          <Button
+                            variant="primary"
+                            className="btn-sm"
+                            style={{
+                              position: "relative",
+                              marginLeft: ".5rem",
+                              marginRight: "1rem",
+                              marginBottom: ".5rem",
+                            }}
+                            onClick={() => deleteHandler(product._id)}
+                          >
+                            Yes, Delete
+                          </Button>
+                        </Popover>
+                      }
+                    >
+                      <Button variant="danger" className="btn-sm">
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </OverlayTrigger>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Paginate pages={pages} page={page} isAdmin={true} />
+        </Fragment>
       )}
     </Fragment>
   );
