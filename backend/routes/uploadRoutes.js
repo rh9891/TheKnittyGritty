2d1b658b20c4e482b37395f1704070934d02bf7a
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-05-11 22:21:19 -0400

    Imported the multer3 dependency to use the s3 upload instead of cloudinary. Waiting for aws details to be authenticated, but have brought in the necessary changes for the upload routes to use aws.

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,41 +1,76 @@
-import path from "path";
+// import path from "path";
 import express from "express";
 import multer from "multer";
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
+import { protect, isAdmin } from "../middleware/authMiddleware";
+
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3();
+aws.config.update({
+  accessKeyID: process.env.AWS_ACCESS_KEY_ID,
+  accessSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-2",
 });
 
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
   } else {
-    cb("Only image files accepted.");
+    cb(
+      new Error("Invalid file type. Only JPEG and PNG image are allowed."),
+      false
+    );
   }
-}
+};
 
 const upload = multer({
-  storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
+  fileFilter,
+  storage: multerS3({
+    acl: "public-read",
+    s3: s3,
+    bucket: `the-knitty-gritty-images`,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: "TESTING_METADATA" });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
 });
 
-router.post("/", upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path}`);
-});
+const returnImageUrl = (req, res) => {
+  return res.send(`${req.file.location}`);
+};
+
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename(req, file, cb) {
+//     cb(
+//       null,
+//       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+//     );
+//   },
+// });
+
+// function checkFileType(file, cb) {
+//   const filetypes = /jpg|jpeg|png/;
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//   const mimetype = filetypes.test(file.mimetype);
+
+//   if (extname && mimetype) {
+//     return cb(null, true);
+//   } else {
+//     cb("Only image files accepted.");
+//   }
+// }
+
+router
+  .route("/")
+  .post(protect, isAdmin, upload.single("image"), returnImageUrl);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
@@ -23,6 +22,10 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Knitty Gritty!");
+});
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -34,18 +37,6 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("Hello. Welcome to The Knitty Gritty.");
-  });
-}
 
 app.use(notFound);
 
