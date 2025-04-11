9ed7b902b08b08fb64af9b84c6e393cab9b65bce
rh9891 <romie.hecdivert@gmail.com>
2025-04-10 23:17:11 -0400

    Added custom bootstrap styles. Added global styles. Added logo image and logo to Header component.

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,12 +1,14 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 
+import TheKnittyGrittyLogo from "../../assets/images/TheKnittyGrittyLogo.png"
+
 const Header = () => {
   return (
     <header>
       <Navbar bg="primary" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">The Knitty Gritty</Navbar.Brand>
+          <Navbar.Brand className="d-flex align-items-center gap-2" href="/"><img src={TheKnittyGrittyLogo} alt="logo" height={24} width={24} /> The Knitty Gritty</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
