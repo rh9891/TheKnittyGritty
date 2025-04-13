5b7dcd62a2be4aed3802456ceca46198d8de57d5
rh9891 <romie.hecdivert@gmail.com>
2025-04-12 23:23:09 -0400

    Modified header and footer component. Created initial home page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
-import Header from "../components/Header.tsx";
-import Footer from "../components/Footer.tsx";
+
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
+import Home from "./screens/Home.tsx";
 
 const App = () => {
   return (
@@ -8,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>The Knitty Gritty</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
