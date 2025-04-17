5d610ce8ac1515570b8ab7a4df7c0c8ab95f807d
rh9891 <romie.hecdivert@gmail.com>
2025-04-17 08:39:29 -0400

    Added database connection with mongoose.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
+
+const port = process.env.PORT || 5005;
 
 dotenv.config();
 
-const port = process.env.PORT || 5005;
+connectDB();
 
 const app = express();
 
