2752139ac04775698421571c0575c898baf7c77a
rh9891 <romie.hecdivert@gmail.com>
2025-04-24 23:58:26 -0700

    Stubbed endpoint for apiSlice to update later.

--- frontend/src/slices/apiSlice.ts
+++ frontend/src/slices/apiSlice.ts
@@ -20,5 +20,9 @@ const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["Product", "Order", "User"],
-  endpoints: (builder: EndpointBuilder) => ({}),
+  endpoints: (builder: EndpointBuilder) => ({
+    placeholder: builder.query<null, void>({
+      query: () => "/",
+    }),
+  }),
 });
