3502501a1407242b860c3d208daa50a88d0e3470
rh9891 <romie.hecdivert@gmail.com>
2025-05-08 22:50:13 -0400

    Added functionality to create and get orders.

--- backend/controllers/orderController.ts
+++ backend/controllers/orderController.ts
@@ -1,25 +1,82 @@
+import { Response } from "express";
+
+import { AuthenticatedRequest } from "../middleware/authMiddleware.js";
+import Order, { OrderItem } from "../models/orderModel.js";
 import asyncHandler from "../middleware/asyncHandler.js";
 
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
-const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("Add order");
-});
+const addOrderItems = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const {
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+      res.status(400);
+      throw new Error(
+        "Your basket is looking a little empty. Add something before checking out!",
+      );
+    } else {
+      const order = new Order({
+        orderItems: orderItems.map((item: OrderItem) => ({
+          ...item,
+          product: item._id,
+          _id: item._id,
+        })),
+        user: req?.user?._id,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+      });
+
+      const createdOrder = await order.save();
+
+      res.status(201).json(createdOrder);
+    }
+  },
+);
 
 // @desc    Get logged-in user's orders
 // @route   GET /api/orders/my-orders
 // @access  Private
-const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("Get my orders");
-});
+const getMyOrders = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const orders = await Order.find({ user: req?.user?._id });
+    res.status(200).json(orders);
+  },
+);
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
-const getOrderById = asyncHandler(async (req, res) => {
-  res.send("Get order by ID");
-});
+const getOrderById = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email",
+    );
+
+    if (order) {
+      res.status(200).json(order);
+    } else {
+      res.status(404);
+      throw new Error(
+        "Hmm...we couldn’t stitch together that order. It may not exist.",
+      );
+    }
+  },
+);
 
 // @desc    Update order to paid
 // @route   GET /api/orders/:id/pay
--- backend/models/orderModel.ts
+++ backend/models/orderModel.ts
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
+export type OrderItem = {
+  _id: string;
+  name: string;
+  quantity: number;
+  image: string;
+  price: number;
+};
+
 const orderSchema = new mongoose.Schema(
   {
     user: {
@@ -10,7 +18,7 @@ const orderSchema = new mongoose.Schema(
     orderItems: [
       {
         name: { type: String, required: true },
-        qty: { type: Number, required: true },
+        quantity: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: Number, required: true },
         product: {
