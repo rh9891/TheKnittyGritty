307bb12e78f7f59ebbd7f4d8ef6515208d10828d
rh9891 <romie.hecdivert@gmail.com>
2025-05-17 03:38:49 -0400

    Added multer dependency and image upload endpoint.

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,21 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import {
   ORDERS_URL,
   PAYPAL_URL,
   PRODUCTS_URL,
+  UPLOAD_URL,
   USERS_URL,
 } from "../frontend/constants.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -34,10 +37,14 @@ app.get("/", (_req: Request, res: Response) => {
 app.use(PRODUCTS_URL, productRoutes);
 app.use(USERS_URL, userRoutes);
 app.use(ORDERS_URL, orderRoutes);
+app.use(UPLOAD_URL, uploadRoutes);
 
 app.get(PAYPAL_URL, (_req: Request, res: Response) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
