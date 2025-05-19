c02f466b7a2f15eb48ed8849738b92e5491c5dbc
rh9891 <romie.hecdivert@gmail.com>
2025-05-18 20:38:21 -0400

    Added backend user routes. Created constant for user not found.

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -10,6 +10,8 @@ export const MISMATCH_ERROR_MESSAGE =
 export const UNPAID_ORDER_MESSAGE =
   "Order is unpaid and has not been processed.";
 export const UNDELIVERED_ORDER_MESSAGE = "Order has not yet been delivered.";
+export const USER_NOT_FOUND_ERROR_MESSAGE =
+  "We looked through every loop and purl. User not found.";
 export const DEFAULT_SHIPPING_ADDRESS = {
   name: "",
   address: "",
