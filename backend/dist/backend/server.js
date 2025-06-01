9051c8880d6f790b881a8ff3627e13398cdf878c
rh9891 <romie.hecdivert@gmail.com>
2025-06-01 04:42:19 -0400

    Downgraded express to resolve v5 missing parameter regex error.

--- backend/dist/backend/server.js
+++ backend/dist/backend/server.js
@@ -26,9 +26,11 @@ app.get(PAYPAL_URL, (_req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "backend/uploads")));
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/build")));
+    const indexPath = path.join(__dirname, "frontend", "dist", "index.html");
+    console.log("📁 indexPath:", indexPath);
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
     app.get("*", (_req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
     });
 }
 else {
